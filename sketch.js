c1cd4eeebacf84dffa84aad0258646b87939674c
MarcusElle <46597619+Marcus4420@users.noreply.github.com>
2019-11-14 08:12:05 +0100

    Tilføjede block til newtons 1.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 let bg;
+let BLOCK;
 
 function preload() {
     bg = loadImage("baggrund.jpg");
+    BLOCK = loadImage("block.png");
 }
 
 function setup() {
@@ -9,6 +11,18 @@ function setup() {
 }
 
 function draw() {
-    createCanvas(500,500);
+    createCanvas(800,800);
     background(bg);
+    spaceblock();
+}
+
+
+function spaceblock() {
+    block = {
+        width: 100,
+        height: 100,
+    }
+
+    image(BLOCK, width/2-block.width/2, height/2-block.height/2, block.width,block.height);
+
 }
