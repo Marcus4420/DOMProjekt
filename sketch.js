ce11e6020a00f03badbe4a8abbfdd8ddb45d54b3
MarcusElle <46597619+Marcus4420@users.noreply.github.com>
2019-11-18 13:46:32 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,10 @@
 let bg;
 let BLOCK;
+let SkubV, SkubOp, SkubNed, SkubHøjre;
+let V;
+let H;
+let O;
+let N;
 
 function preload() {
     bg = loadImage("baggrund.jpg");
@@ -7,13 +12,29 @@ function preload() {
 }
 
 function setup() {
-
+    SkubV = createSlider(0, 10, 0);
+    SkubV.position(20, 50);
+    SkubHøjre = createSlider(0, 10, 0);
+    SkubHøjre.position(20, 100);
+    SkubOp = createSlider(0, 10, 0);
+    SkubOp.position(20, 150);
+    SkubNed = createSlider(0, 10, 0);
+    SkubNed.position(20, 200);
 }
 
 function draw() {
     createCanvas(800,800);
     background(bg);
     spaceblock();
+    let V = SkubV.value();
+    let H = SkubHøjre.value();
+    let O = SkubOp.value();
+    let N = SkubNed.value();
+    fill(255);
+    text('→ Newton: ' + V, SkubV.x * 2, 50);
+    text('← Newton: ' + H, SkubHøjre.x * 2, 100);
+    text('↑ Newton: ' + O, SkubOp.x * 2, 150);
+    text('↓ Newton: ' + N, SkubNed.x * 2, 200);
 }
 
 
@@ -21,8 +42,15 @@ function spaceblock() {
     block = {
         width: 100,
         height: 100,
+        x: 350,
+        y: 350,
     }
 
-    image(BLOCK, width/2-block.width/2, height/2-block.height/2, block.width,block.height);
+    image(BLOCK, block.x, block.x, block.width,block.height);
 
+    //CONTROLS//
+
+    block.x = block.x + N;
 }
+
+
